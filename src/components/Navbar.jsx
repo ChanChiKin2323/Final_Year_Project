12fc4ed58b87f00df7bedb5864c99cf4b814cb56
Chan6758 <chentan338@gmail.com>
2026-09-19 14:38:18 +0800

    fix a bug that the interface cannot show

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, TicketPlus, XIcon, useState } from 'lucide-react'
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
+import { useState } from 'react'
 import { useClerk, UserButton, useUser } from '@clerk/react'
 
 const Navbar = () => {
